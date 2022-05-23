0425324db3a61950c39ceca622e8a5650ebcc39f
David <davidrhodenbaugh97@gmail.com>
2022-05-23 15:47:29 -0500

    ah

--- yaga.js
+++ yaga.js
@@ -1,3 +1,33 @@
+//2
+function minMax(arr) {
+	//return [Math.min(arr),Math.max(arr)]
+	return [Math.min(...arr),Math.max(...arr)]
+}
+
+//3
+function FirstReverse(str) {
+  return str.split("").reverse().join("");
+  return str;
+
+}
+
+//4
+function LongestWord(sen) {
+    let word = sen.match(/\w+/g);
+
+    let longest = word[0];
+
+     for (i=0; i < word.length; i++) {
+         if (word[i].length > longest.length) {
+             longest = word[i];
+         }
+     }
+
+    return longest;
+
+}
+
+//5
 function addUp(num) {
 	let ans = 0;
 	for (i = 1; i <= num; i++) {
